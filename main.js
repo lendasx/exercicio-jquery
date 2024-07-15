2171e50072bc7f71ebb1e6733f9f06d4ae8ecd80
Lucas Geraldo rosa <seu-email@example.com>
2024-07-15 12:14:50 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    function addTask(taskName) {
+    $('#taskList').append('<tr class="sortable-item"><td class="checkbox-col"><input type="checkbox"></td><td>' + taskName + '</td><td class="drag-handle">&#9776;</td></tr>');
+    }
+
+    $('#taskForm').submit(function(event) {
+    event.preventDefault();
+    var taskName = $('#taskInput').val();
+
+        if (taskName.trim() !== '') {
+        addTask(taskName);
+        $('#taskInput').val('');
+        makeTasksSortable();
+}
+    });
+
+    $('#taskList').on('change', ':checkbox', function() {
+    $(this).closest('tr').find('td:nth-child(2)').toggleClass('completed');
+    });
+
+    $('#cancelButton').click(function() {
+    $('#taskInput').val('');
+    });
+
+    function makeTasksSortable() {
+    $('.sortable').sortable({
+        handle: '.drag-handle',
+        update: function(event, ui) {
+        }
+    });
+    }
+
+    makeTasksSortable();
+});
